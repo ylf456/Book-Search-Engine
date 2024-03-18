a0d680c9b2d74a9190f88c285a5d472ddba422f4
ylf456 <145236890+ylf456@users.noreply.github.com>
2024-03-18 16:10:21 -0700

    staging changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,12 +7,25 @@ import {
   createHttpLink,
 } from '@apollo/client';
 
+import { setContext } from '@apollo/client/link/context';
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
 const client = new ApolloClient({
- // link: authLink.concat(httpLink),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
