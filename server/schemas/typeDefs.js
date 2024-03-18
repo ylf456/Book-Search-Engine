7ec290c0539834aee98d6517f2e5519d85c5cd3e
ylf456 <145236890+ylf456@users.noreply.github.com>
2024-03-17 23:05:08 -0700

    added resolver+typeDef and queries+mutations for the DB and graphql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,39 @@
-const typeDefs
+const typeDefs = `
+type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [bookSchema]
+}
+
+type bookSchema {
+    authors: String
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+type Auth {
+    token: ID!
+    user: User
+  }
+
+type Query {
+    getSingleUser(userID:ID!)
+}
+
+type Mutation {
+   createUser(name: String!, email: String!, password: String!): Auth
+   login(email: String!, password: String!): Auth
+   saveBook(authors: String
+    description: String!,
+    bookId: String!,
+    image: String,
+    title: String!): User
+   deleteBook(bookId:String!): User
+}
+`
+
+module.exports = typeDefs;
